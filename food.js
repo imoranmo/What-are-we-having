629e78ac5597cce95c1556161302226bd8d5d3d8
imoranmo <imoranmo@gmail.com>
2021-07-23 18:49:04 -0700

    button work

--- food.js
+++ food.js
@@ -1,12 +1,17 @@
 var foodName = document.querySelector('.foodname')
 var foodSummary = document.querySelector('.foodsummary')
 var submitBtn = document.querySelector ('#submit')
+var foodsave = document.querySelector ('#list')
+var foodsaveBtn = document.querySelector('#foodsavebtn')
+var foodarray =[]
 
 function dietRestrict() {
   var f= document.getElementById("food");
   var d= document.getElementById("Drink");
   var result = f.options[f.selectedIndex].value;
   var result2 = d.options[d.selectedIndex].value;
+  const firstPage = document.querySelector(".menu");
+  const mainPage = document.querySelector(".main-page");
   
   if(result === 'Select...' || result2 === 'Select...'){
     return
@@ -17,7 +22,15 @@ function dietRestrict() {
 
     diet = result
   }
-   
+
+  if (firstPage.classList.contains("show")) {
+      firstPage.classList.remove("show");
+      firstPage.classList.add("hide");
+  }
+  if (mainPage.classList.contains("hide")) {
+      mainPage.classList.remove("hide");
+      mainPage.classList.add("show");
+  }
 
   foodApi(diet)
 
@@ -48,23 +61,25 @@ function foodApi(diet) {
         link.title = data.recipes[0].title;
         link.href = data.recipes[0].sourceUrl;
         foodName.appendChild(link);
+        var title = data.recipes[0].title.trim();
+        foodarray.push(title)
 
+        console.log(foodarray)
       });
   }
   
+ function saveRecipe() {
 
+  var foodlength = foodarray.length
+  console.log(foodarray)
+  var title = foodarray[foodlength]
+  console.log(foodlength)
+  console.log(title)
+  var newfood= document.createElement('button')
+  newfood.textContent = title;
+  foodsave.appendChild(newfood);
 
-  submitBtn.addEventListener('click', dietRestrict)
+ };
 
-  submit.onclick = () => {
-    const firstPage = document.querySelector(".menu");
-    const mainPage = document.querySelector(".main-page");
-    if (firstPage.classList.contains("show")) {
-        firstPage.classList.remove("show");
-        firstPage.classList.add("hide");
-    }
-    if (mainPage.classList.contains("hide")) {
-        mainPage.classList.remove("hide");
-        mainPage.classList.add("show");
-    }
-  }
+submitBtn.addEventListener('click', dietRestrict)
+foodsaveBtn.onclick = saveRecipe()
